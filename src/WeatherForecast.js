3663cbdea7c0d2b0dfe099c8cb6da72c990675a3
lscalise17 <97422956+lscalise17@users.noreply.github.com>
2022-05-23 21:53:16 -0500

    typed weatherforecast data

---
+++ src/WeatherForecast.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+import WeatherForecastDays from "./WeatherForecastDays";
+import axios from "axios";
+import WeatherForecastDays from "./WeatherForecastDays";
+
+export default function WeatherForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
+
+  function handleResponse(response) {
+    setForecast(response.data.daily);
+    setLoaded(true);
+  }
+
+  function load() {
+    let apiKey = "247901f5cf32f02ac75b3bd33daf80d0";
+    let longitude = props.coordinate.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+  if (loaded) {
+    return (
+      <div className="WeatherForecasr">
+        <div className="row">
+          <div className="col">
+            <WeatherForecastDays data={forecast[0]} />
+          </div>
+          <div className="col">
+            <WeatherForecastDays data={forecast[1]} />
+          </div>
+          <div className="col">
+            <WeatherForecastDays data={forecast[2]} />
+          </div>
+          <div className="col">
+            <WeatherForecastDays data={forecast[3]} />
+          </div>
+          <div className="col">
+            <WeatherForecastDays data={forecast[5]} />
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    load();
+  }
+}
